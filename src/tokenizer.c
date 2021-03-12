7e70a8bedc9f56b39e72daa638209882a3c6cbfb
student <daquinonesr@miners.utep.edu>
2021-03-12 09:28:13 -0700

    Finished tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -42,3 +42,79 @@ int non_space_char(char c)
   }
   return 1;
 }
+int count_words(char *str)
+{
+  int wordCounter=0;
+  char *p=word_start(str);
+  while(*p!='\0')
+  {
+    if(non_space_char(*p)){
+      wordCounter+=1;
+    }
+    p=word_start(p);
+    p=word_terminator(p);
+  }
+  return wordCounter;
+}
+char *copy_str(char *inStr, short len)
+{
+  char *scopy=malloc((len+1)*sizeof(char));
+  int x;
+  for(x=0;x<len;x++){
+    scopy[x]=inStr[x];
+  }
+  scopy[x]='\0';
+  return scopy;
+}
+char **tokenize(char* star)
+{
+  int numWords=count_words(str);
+  char **tokens=malloc((numWords+1)*sizeof(char *));
+  char *p=starl
+  int x;
+  for(x=0;x<numWords;x++)
+  {
+    p=word_start(p);
+    int length = word_length(p);
+    tokens[x]=copy_str(p,length);
+    p=word_terminator(p);
+  }
+  tokens[x]=0;
+  return tokens;
+}
+void free_tokens(char **tokens)
+{
+  int x;
+  for(x=0;tokens[x];!=0;x++)
+    {
+      free(tokens[x]);
+    }
+  free(tokens);
+}
+short word_length(char *str)
+{
+  short length=0;
+  int x;
+  for(x=0;*(str+x)!='\0';x++)
+    {
+      if(non_space_char(*(str+x)))
+	{
+	  length+=1;
+	}
+      else
+	{
+	  break:
+	}
+    }
+  return length;
+}
+short string_length(char *str)
+{
+  short length=0;
+  int x;
+  for(x=0;*(str+x)!='\0';x++)
+    {
+      length+=1;
+    }
+  return length;
+}
